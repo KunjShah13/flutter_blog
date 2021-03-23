de76f7311ccefac72b134dd8c0ffefec28226fa6
Kunj Shah <KunjShah13@users.noreply.github.com>
2021-03-23 23:19:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/views/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  somethingWentWrong() {
+    print("Something went wrong.");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return somethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: ThemeData.dark(),
+            home: HomePage(),
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
+      },
+    );
+  }
+}
---
+++ lib/views/create_blog.dart
@@ -0,0 +1,170 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/services/crud.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
+
+class CreateBlog extends StatefulWidget {
+  @override
+  _CreateBlogState createState() => _CreateBlogState();
+}
+
+class _CreateBlogState extends State<CreateBlog> {
+  String authorName, title, desc;
+
+  File selectedImage;
+  bool _isLoading = false;
+  CrudMethods crudMethods = new CrudMethods();
+
+  Future getImage() async {
+    final pickedFile = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        selectedImage = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  uploadBlog() async {
+    if (selectedImage != null) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      ///upload image to firebase storage
+
+  FirebaseStorage storage = FirebaseStorage.instance;
+      Reference ref = storage
+          .ref()
+          .child("blogImages")
+          .child("${randomAlphaNumeric(9)}.jpg");
+
+      final UploadTask task = ref.putFile(selectedImage);
+
+      var downloadUrl = await (await task.whenComplete(() => null)).ref.getDownloadURL();
+      print("this is URL $downloadUrl");
+
+      Map<String, String> blogMap = {
+        "imgURL": downloadUrl,
+        "authorName": authorName,
+        "title": title,
+        "desc": desc
+      };
+
+      crudMethods.addData(blogMap).then((result) {
+        Navigator.pop(context);
+      });
+      } else {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter",
+              style: TextStyle(fontSize: 22),
+            ),
+            Text(
+              "Blog",
+              style: TextStyle(fontSize: 22, color: Colors.blue),
+            )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        actions: <Widget>[
+          GestureDetector(
+            onTap: () {
+              uploadBlog();
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Icon(Icons.file_upload),
+            ),
+          )
+        ],
+      ),
+      body: _isLoading
+          ? Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      getImage();
+                    },
+                    child: selectedImage != null
+                        ? Container(
+                            margin: EdgeInsets.symmetric(horizontal: 16),
+                            height: 170,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(6)),
+                            child: ClipRRect(
+                              child: Image.file(
+                                selectedImage,
+                                fit: BoxFit.cover,
+                              ),
+                            ))
+                        : Container(
+                            margin: EdgeInsets.symmetric(horizontal: 16),
+                            height: 170,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(6)),
+                            width: MediaQuery.of(context).size.width,
+                            child: Icon(
+                              Icons.add_a_photo,
+                              color: Colors.black45,
+                            ),
+                          ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 16),
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(hintText: "Author Name"),
+                          onChanged: (val) {
+                            authorName = val;
+                          },
+                        ),
+                        TextField(
+                          decoration: InputDecoration(hintText: "Title Name"),
+                          onChanged: (val) {
+                            title = val;
+                          },
+                        ),
+                        TextField(
+                          decoration: InputDecoration(hintText: "Description"),
+                          onChanged: (val) {
+                            desc = val;
+                          },
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+    );
+  }
+}
